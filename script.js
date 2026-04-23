4e21c9fbba991a46b16f76a2f8d61f70b82618bd
Alvin Chou <alvinwchou@gmail.com>
2026-04-23 05:52:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const GOOGLE_SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS
 // 2) WRITE (SheetDB API Base URL)
 // Example format:
 // https://sheetdb.io/api/v1/abc123xyz
-const SHEETDB_API_BASE_URL = 'https://sheetdb.io/api/v1/k26l5mm694tum';
+const SHEETDB_API_BASE_URL = 'https://sheetdb.io/api/v1/3jkmbf9vbhkf0';
 
 // Column name used to uniquely identify rows for updates.
 // Must match your sheet header exactly.
